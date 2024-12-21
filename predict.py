07f87afd2a3514636227d3e847826ef68f647ca7
Nagendra320 <nagendranagi425@gmail.com>
2024-12-21 07:58:28 +0100

    loan prediction and deploy by flask

---
+++ predict.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import pickle
+import pandas
+import numpy 
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/ping', methods=['GET'])
+def pinger():
+    return f"Hello i am here!!"
+
+model = open("./artefacts/classifier.pkl", "rb")
+clf = pickle.load(model)
+
+@app.route("/predict", methods=['POST'])
+def prediction():
+    loan_req = request.get_json()
+
+    if loan_req['Gender'] == 'Male':
+        Gender = 0
+    else:
+        Gender = 1
+
+    if loan_req['Married'] == 'Unmarried':
+        Married = 0
+    else:
+        Married = 1
+
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1
+    
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount']
+
+
+    result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+
+    if result == 1:
+        pred = 'Approved'
+    else:
+        pred = 'Rejected'
+
+    return {"loan_approval_status": pred}
