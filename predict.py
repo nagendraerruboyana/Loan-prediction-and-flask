29a6da5eecb455e78cfc82cdae922cb237c02445
Nagendra320 <nagendranagi425@gmail.com>
2024-12-27 16:44:03 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return {"MESSAGE" : "Hello, World......!!!!!"}
+    return {"MESSAGE" : "Hello, World......!!!!!!!"}
 
 @app.route('/ping', methods=['GET'])
 def pinger():
